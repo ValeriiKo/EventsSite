2448f1e886fe9e3b66a57a9702413e02cf2e7763
Valerii <valeriikozhevnikov@gmail.com>
2017-09-11 11:26:23 +0300

    Добавил NinjectFactory

---
+++ WUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace WUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel=new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                ? null
+                : (IController) ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            
+        }
+    }
+}
