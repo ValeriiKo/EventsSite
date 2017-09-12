cd3f50f1fa04e7707fe9c7e0d0468595f787bde5
Valerii <valeriikozhevnikov@gmail.com>
2017-09-12 12:01:16 +0300

    Исправил доступ класса на public

--- Domain/Entities/Events.cs
+++ Domain/Entities/Events.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class Events
+    public class Events
     {
         public int EventId { get; set; }
         public string Name { get; set; }
