6fabccb96055c4f9239f546a384a154fb28cb641
Alexey <kadomtsef@gmail.com>
2017-09-12 11:41:00 +0300

    create a domain model

---
+++ Domain/Entities/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Events
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public DateTime BeginDate { get; set; }
+        public string Description { get; set; }
+    }
+}
