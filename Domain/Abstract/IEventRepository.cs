31c3fa8cf933b044952f7e7c43db25f15baaec5d
Valerii <valeriikozhevnikov@gmail.com>
2017-09-14 17:06:25 +0300

    Создание абстрактного и иммитированного хранилищ.

---
+++ Domain/Abstract/IEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using Domain.Entities;
+
+namespace Domain.Abstract
+{
+    public interface IEventRepository
+    {
+        IQueryable<Event> Events { get; }
+    }
+}
--- WUI/Infrastructure/NinjectControllerFactory.cs
+++ WUI/Infrastructure/NinjectControllerFactory.cs
@@ -3,6 +3,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Ninject;
+using Moq;
+using System.Linq;
+using System.Collections.Generic;
+using Domain.Entities;
+using Domain.Abstract;
 
 namespace WUI.Infrastructure
 {
@@ -25,7 +30,15 @@ namespace WUI.Infrastructure
 
         private void AddBindings()
         {
-            
+            Mock<IEventRepository> mock=new Mock<IEventRepository>();
+            mock.Setup(m => m.Events).Returns(new List<Event>
+            {
+                new Event {Name = "Soccer game", Description = "Zenit-Spartak"},
+                new Event {Name = "Day of cinema", Description = "Cinema festival"},
+                new Event {Name = "Race", Description = "Participants: Ferrary, Lambordginy, Mercedes"}
+            }.AsQueryable());
+
+            ninjectKernel.Bind<IEventRepository>().ToConstant(mock.Object);
         }
     }
 }
